b3c3c7f252cc9791051826224262ffdcbad230d6
AndySarango55 <113343526+AndySarango55@users.noreply.github.com>
2023-01-17 13:08:57 +0100

    ejercicio 1 terminado

--- ejercicio 1.py
+++ ejercicio 1.py
@@ -1,3 +1,5 @@
+import datetime
+
 
 def fibonacci(numero):
     if (numero == 0):
@@ -6,7 +8,35 @@ def fibonacci(numero):
         return 1
     else:
         return (fibonacci(numero-2)+fibonacci(numero-1))
+inicio = datetime.datetime.now()
 numero = int(input("Introduce el valor del numero:\n "))
 print("La secuencia es:")
 for numero in range(0, numero):
   print(fibonacci(numero))
+
+fin = datetime.datetime.now()
+print("el tiempo de ejecucion es de :", fin - inicio)
+
+
+def fibonacci_iter(n):
+    a = 1
+    b = 1
+    if n == 1:
+        print('0')
+    elif n == 2:
+        print('0', '1')
+    else:
+        print('0')
+        print(a)
+        print(b)
+        for i in range(n - 3):
+            total = a + b
+            b = a
+            a = total
+            print(total)
+
+inicio2 = datetime.datetime.now()
+fibonacci_iter(8)
+fin = datetime.datetime.now()
+print("el tiempo de ejecucion es de :", fin - inicio)
+
